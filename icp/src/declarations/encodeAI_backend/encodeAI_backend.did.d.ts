eeabb75804e767a9e6dd0e48a9ee7ed468b2438e
acgodson <anigodson20@gmail.com>
2024-07-01 07:11:36 +0100

    add declarations

---
+++ icp/src/declarations/encodeAI_backend/encodeAI_backend.did.d.ts
@@ -0,0 +1,83 @@
+import type { Principal } from '@dfinity/principal';
+import type { ActorMethod } from '@dfinity/agent';
+import type { IDL } from '@dfinity/candid';
+
+export interface CanisterHttpResponsePayload {
+  'status' : bigint,
+  'body' : Uint8Array | number[],
+  'headers' : Array<HttpHeader>,
+}
+export type FileId = string;
+export type FileId__1 = string;
+export interface FileInfo {
+  'name' : string,
+  'size' : bigint,
+  'fileId' : string,
+  'chunkCount' : bigint,
+}
+export interface HttpHeader { 'value' : string, 'name' : string }
+export interface HttpResponsePayload {
+  'status' : bigint,
+  'body' : Uint8Array | number[],
+  'headers' : Array<HttpHeader>,
+}
+export interface Main {
+  'addDocument' : ActorMethod<
+    [string, string],
+    [[] | [Principal], [] | [string]]
+  >,
+  'addVector' : ActorMethod<
+    [Principal, string, string, bigint, bigint, Array<number>],
+    [] | [string]
+  >,
+  'closeProposal' : ActorMethod<[bigint], undefined>,
+  'createEmbeddings' : ActorMethod<[string], string>,
+  'createProposal' : ActorMethod<
+    [string, Array<Uint8Array | number[]>, bigint],
+    bigint
+  >,
+  'deployDIP20' : ActorMethod<[], Principal>,
+  'documentIDToTitle' : ActorMethod<[string], [] | [string]>,
+  'getChunk' : ActorMethod<[FileId__1, bigint, Principal], [] | [string]>,
+  'getChunks' : ActorMethod<[FileId__1, Principal], [] | [string]>,
+  'getDIP20Principal' : ActorMethod<[], [] | [Principal]>,
+  'getDocumentID' : ActorMethod<[string, Principal], [] | [string]>,
+  'getIndexInfo' : ActorMethod<[Principal], [] | [Array<FileInfo>]>,
+  'getPrincipal' : ActorMethod<[], [] | [string]>,
+  'getProposalStatus' : ActorMethod<
+    [bigint],
+    { 'status' : [] | [ProposalState] }
+  >,
+  'getVectors' : ActorMethod<
+    [Principal],
+    [] | [{ 'items' : Array<VectorData> }]
+  >,
+  'mintToken' : ActorMethod<[], TxReceipt>,
+  'titleToDocumentID' : ActorMethod<[string], [] | [string]>,
+  'transform' : ActorMethod<[TransformArgs], CanisterHttpResponsePayload>,
+  'vote' : ActorMethod<[bigint, boolean], undefined>,
+  'wallet_receive' : ActorMethod<[], undefined>,
+}
+export type ProposalState = { 'active' : null } |
+  { 'cancelled' : null } |
+  { 'approved' : null };
+export interface TransformArgs {
+  'context' : Uint8Array | number[],
+  'response' : HttpResponsePayload,
+}
+export type TxReceipt = { 'ok' : bigint } |
+  {
+    'err' : { 'InsufficientAllowance' : null } |
+      { 'InsufficientBalance' : null } |
+      { 'Unauthorized' : null }
+  };
+export interface VectorData {
+  'startPos' : bigint,
+  'vectorId' : string,
+  'vector' : Array<number>,
+  'documentId' : FileId,
+  'endPos' : bigint,
+}
+export interface _SERVICE extends Main {}
+export declare const idlFactory: IDL.InterfaceFactory;
+export declare const init: (args: { IDL: typeof IDL }) => IDL.Type[];
---
+++ icp/src/declarations/encodeAI_backend/encodeAI_backend.did.js
@@ -0,0 +1,112 @@
+export const idlFactory = ({ IDL }) => {
+  const FileId__1 = IDL.Text;
+  const FileInfo = IDL.Record({
+    'name' : IDL.Text,
+    'size' : IDL.Nat,
+    'fileId' : IDL.Text,
+    'chunkCount' : IDL.Nat,
+  });
+  const ProposalState = IDL.Variant({
+    'active' : IDL.Null,
+    'cancelled' : IDL.Null,
+    'approved' : IDL.Null,
+  });
+  const FileId = IDL.Text;
+  const VectorData = IDL.Record({
+    'startPos' : IDL.Int,
+    'vectorId' : IDL.Text,
+    'vector' : IDL.Vec(IDL.Float64),
+    'documentId' : FileId,
+    'endPos' : IDL.Int,
+  });
+  const TxReceipt = IDL.Variant({
+    'ok' : IDL.Nat,
+    'err' : IDL.Variant({
+      'InsufficientAllowance' : IDL.Null,
+      'InsufficientBalance' : IDL.Null,
+      'Unauthorized' : IDL.Null,
+    }),
+  });
+  const HttpHeader = IDL.Record({ 'value' : IDL.Text, 'name' : IDL.Text });
+  const HttpResponsePayload = IDL.Record({
+    'status' : IDL.Nat,
+    'body' : IDL.Vec(IDL.Nat8),
+    'headers' : IDL.Vec(HttpHeader),
+  });
+  const TransformArgs = IDL.Record({
+    'context' : IDL.Vec(IDL.Nat8),
+    'response' : HttpResponsePayload,
+  });
+  const CanisterHttpResponsePayload = IDL.Record({
+    'status' : IDL.Nat,
+    'body' : IDL.Vec(IDL.Nat8),
+    'headers' : IDL.Vec(HttpHeader),
+  });
+  const Main = IDL.Service({
+    'addDocument' : IDL.Func(
+        [IDL.Text, IDL.Text],
+        [IDL.Opt(IDL.Principal), IDL.Opt(IDL.Text)],
+        [],
+      ),
+    'addVector' : IDL.Func(
+        [
+          IDL.Principal,
+          IDL.Text,
+          IDL.Text,
+          IDL.Nat,
+          IDL.Nat,
+          IDL.Vec(IDL.Float64),
+        ],
+        [IDL.Opt(IDL.Text)],
+        [],
+      ),
+    'closeProposal' : IDL.Func([IDL.Nat], [], []),
+    'createEmbeddings' : IDL.Func([IDL.Text], [IDL.Text], []),
+    'createProposal' : IDL.Func(
+        [IDL.Text, IDL.Vec(IDL.Vec(IDL.Nat8)), IDL.Nat],
+        [IDL.Nat],
+        [],
+      ),
+    'deployDIP20' : IDL.Func([], [IDL.Principal], []),
+    'documentIDToTitle' : IDL.Func([IDL.Text], [IDL.Opt(IDL.Text)], []),
+    'getChunk' : IDL.Func(
+        [FileId__1, IDL.Nat, IDL.Principal],
+        [IDL.Opt(IDL.Text)],
+        [],
+      ),
+    'getChunks' : IDL.Func([FileId__1, IDL.Principal], [IDL.Opt(IDL.Text)], []),
+    'getDIP20Principal' : IDL.Func([], [IDL.Opt(IDL.Principal)], ['query']),
+    'getDocumentID' : IDL.Func(
+        [IDL.Text, IDL.Principal],
+        [IDL.Opt(IDL.Text)],
+        [],
+      ),
+    'getIndexInfo' : IDL.Func(
+        [IDL.Principal],
+        [IDL.Opt(IDL.Vec(FileInfo))],
+        [],
+      ),
+    'getPrincipal' : IDL.Func([], [IDL.Opt(IDL.Text)], []),
+    'getProposalStatus' : IDL.Func(
+        [IDL.Nat],
+        [IDL.Record({ 'status' : IDL.Opt(ProposalState) })],
+        [],
+      ),
+    'getVectors' : IDL.Func(
+        [IDL.Principal],
+        [IDL.Opt(IDL.Record({ 'items' : IDL.Vec(VectorData) }))],
+        [],
+      ),
+    'mintToken' : IDL.Func([], [TxReceipt], []),
+    'titleToDocumentID' : IDL.Func([IDL.Text], [IDL.Opt(IDL.Text)], []),
+    'transform' : IDL.Func(
+        [TransformArgs],
+        [CanisterHttpResponsePayload],
+        ['query'],
+      ),
+    'vote' : IDL.Func([IDL.Nat, IDL.Bool], [], []),
+    'wallet_receive' : IDL.Func([], [], []),
+  });
+  return Main;
+};
+export const init = ({ IDL }) => { return []; };
